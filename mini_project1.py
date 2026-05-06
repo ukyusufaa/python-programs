3017b46bc294bc4fbf5858a3878da6a29bff4430
yusuf <ukyusufaa@gmail.com>
2026-05-06 21:27:26 +0100

    Updated student program logic

--- mini_project1.py
+++ mini_project1.py
@@ -1,94 +1,179 @@
-def validate_mark(marks):
-    if 0 <= marks <= 100:
-        return marks
+def menu():
+    print("\nTRINITY UPPER SCHOOL 1993\nNORTHAMPTON")
+    print("1:Insert Students")
+    print("2:Display Students")
+    print("3:Search Students")
+    print("4.Delete Students")
+    print("5.Exit")
+
+def validate_marks(marks_list):
+    for mark in marks_list:
+        if mark < 0 or mark > 100:
+            return False
+    return True
+
+def find_average_mark(marks_list):
+    total = 0
+
+    for mark in marks_list:
+        total += mark
+    average = total /len(marks_list)
+    return average
+
+def final_grade(avg):
+    if avg < 10:
+        return "U"
+    elif avg < 25:
+        return "F"
+    elif avg < 30:
+        return "E"
+    elif avg < 40:
+        return "D"
+    elif avg < 50:
+        return "C"
+    elif avg < 65:
+        return "B"
+    elif avg < 75:
+        return "A"
     else:
-        return None 
+        return "A*"
 
-def avg_mark(marks):
-    total=0
-    for mark in marks:
-        total+=mark
-    avg=total/len(marks)
-    return avg
+def find_lowest_student(students_list):
+    lowest = None
 
-def grade(avg):
-    if avg <= 40:
-        return "FAIL"
-    elif avg <= 59:
-        return "THIRD CLASS - 3rd"
-    elif avg <= 65:
-        return "SECOND CLASS LOWER DIVISION - BSc(HONs) COMPUTER COMMUNICATIONS (2ii)"
-    elif avg <= 79:
-        return "SECOND CLASS HIGHER DIVISION - BSc(HONs) COMPUTER COMMUNICATIONS (2i)"
-    else: 
-        return "FIRST CLASS DEGREE- BSc(HONs) COMPUTER COMUNICATIONS (1st)"
+    for student in students_list:
+        if lowest is None or student["Average"] < lowest["Average"]:
+            lowest = student
+    return lowest
 
-def lowest_mark_student(students):
-    lowest_mark=None
-    for student in students:
-        avg=student["Average Mark"]
-        if lowest_mark is None or avg < lowest_mark["Average Mark"]:
-            lowest_mark=student
-    return lowest_mark
+def find_highest_student(students_list):
+    highest = None 
 
-def highest_mark_student(students):
-    highest_mark=None 
-    for student in students:
-        avg=student["Average Mark"]
-        if highest_mark is None or avg > highest_mark["Average Mark"]:
-            highest_mark=student
-    return highest_mark
+    for student in students_list:
+        if highest is None or student["Average"] > highest["Average"]:
+            highest = student
+    return highest
 
-def main():
-    students=[]
+def insert_student(students_list):
+    name = input("Name:")
+    dob = input("DOB:")
 
-    n=int(input("How many students?"))
-    for i in range(n):
-        name=input("Student name:")
-        dob=input("DOB:")
+    marks_list = []
 
-        all_marks=[]
-
-        for j in range(7):
-            while True:
-                    try:
-                        marks=int(input(f"Paper {j+1}:"))
-                        xyz=validate_mark(marks)
-                        if xyz is not None:
-                            all_marks.append(marks)
-                            break
-                        else:
-                            print("Invalid mark entered(Enter BETWEEN 0-100)")
-                    except ValueError:
-                        print("Error(Enter 'Numbers' only)")
-        
-        avg=avg_mark(all_marks)
-        result=grade(avg)
-      
-        
-        students.append({
-            "Name":name,
-            "DOB":dob,
-            "Total Marks":all_marks,
-            "Average Mark":avg,
-            "Grade":result,
+    while True:
+        try:
+            user_input = input("Enter 5 marks:")
+            marks_list = list(map(int, user_input.split()))
+            if len(marks_list) != 5:
+                print("ERROR - Enter exactly 5 marks")
+                continue 
+            if not validate_marks(marks_list):
+                print("ERROR - Enter marks between 0 - 100")
+                continue 
+            break
+        except ValueError:
+            print("ERROR - Do NOT enter characters - Enter ONLY numbers")
+            continue 
     
-        })
-    lowest=lowest_mark_student(students)
-    highest=highest_mark_student(students)
+    av = find_average_mark(marks_list)
+    grade = final_grade(av)
 
-    for info in students:
-        print(f"\nName:{info['Name']}")
-        print(f"DOB:{info['DOB']}")
-        print(f"Marks:{info['Total Marks']}")
-        print(f"Average:{info['Average Mark']}")
-        print(f"Final Grade:{info['Grade']}")
+    students_list.append({
+        "Name":name,
+        "DOB": dob,
+        "Average":av,
+        "Grade":grade
+    })
+    print("Student inserted sucessfully")
 
-    print("\n---SUMMARY---")
-    print(f"Lowest Student:{lowest['Name']} ({lowest['Average Mark']})")
-    print(f"Highest Student:{highest['Name']} ({highest['Average Mark']})")
+def display_students(students_list):
+    if len(students_list) == 0:
+        print("No students found - Please first insert students and then search")
+        return
+    
+    for s in students_list:
+        print(f"Name:{s['Name']}")
+        print(f"DOB:{s['DOB']}")
+        print(f"Final Mark(Average):{s['Average']}")
+        print(f"Grade:{s['Grade']}")
+    
+    lwt_student = find_lowest_student(students_list)
+    hgt_student = find_highest_student(students_list)
 
-main()
+    print(f"Name:{lwt_student['Name']} Lowest Mark:{lwt_student['Average']}")
+    print(f"Name:{hgt_student['Name']} Highest Mark:{hgt_student['Average']}")
+
+def search_students(students_list):
+    found = False
+
+    print("Student search engine")
+    user_input = input("Student name:").strip().lower()
+    for student in students_list:
+        if student["Name"].strip().lower() == user_input:
+            found = True
+            print(student)
+    if found == True:
+        print("Student sucessfully found")
+    else:
+        print("No student found")
+
+def delete_students(students_list):
+    found = False
+
+    print("Delete a student")
+    user_input = input("Student name:").strip().lower()
+
+    for student in students_list:
+        if student['Name'].strip().lower() == user_input:
+            students_list.remove(student)
+            found = True
+            print(f"Student:{student['Name']},successfully deleted")
+            break 
+    if not found:
+        print("ERROR - Please re-enter correct name\nor\nList Empty ")
+        
+def choosing(students_list):
+    while True:
+        menu()
+
+        user_choice = input("Enter a choice from the menu:")
+
+        if user_choice == "1":
+            insert_student(students_list)
+        elif user_choice == "2":
+            display_students(students_list)
+        elif user_choice == "3":
+            search_students(students_list)
+        elif user_choice == "4":
+            delete_students(students_list)
+        elif user_choice == "5":
+            print("Kindest Regards, GoodBye.\nHead Master MSc BSc DipICT Yusuf Ahmad Ali ")
+            break 
+        else:
+            print("ERROR - Invalid selection- Try again")
+            
+# IMPORTANT WITHOUT THIS PROGRAM WILL NOT RUN
+all_students = []
+choosing(all_students)
+        
+
+
+
+
+
+
+            
+
+
+
+
+    
+ 
+
+
+
+
+
 
 
 
--- yusuf.py
+++ yusuf.py
@@ -1,117 +1,165 @@
 def menu():
-    print("TRINITY UPPER SCHOOL 1993")
-    print("1.Insert Students")
-    print ("2.Display Students")
-    print("Exit")
+    print("\nTRINITY UPPER SCHOOL 1993\nNORTHAMPTON")
+    print("1:Insert Students")
+    print("2:Display Students")
+    print("3:Search Students")
+    print("4.Delete Students")
+    print("5.Exit")
 
 def validate_marks(marks_list):
-    for marks in marks_list:
-        if marks < 0 or marks > 100:
-            return False 
+    for mark in marks_list:
+        if mark < 0 or mark > 100:
+            return False
     return True
 
-      
-def find_average_marks(marks_list):
+def find_average_mark(marks_list):
     total = 0
-    for marks in marks_list:
-        total += marks
-    average = total / len(marks_list)
+
+    for mark in marks_list:
+        total += mark
+    average = total /len(marks_list)
     return average
 
-def final_grades(average):
-    if average <= 30:
-        return "FAIL"
-    elif average <= 45:
+def final_grade(avg):
+    if avg < 10:
+        return "U"
+    elif avg < 25:
+        return "F"
+    elif avg < 30:
         return "E"
-    elif average <= 50:
+    elif avg < 40:
         return "D"
-    elif average <= 55:
+    elif avg < 50:
         return "C"
-    elif average <=70:
+    elif avg < 65:
         return "B"
-    else:
+    elif avg < 75:
         return "A"
+    else:
+        return "A*"
 
-def insert_students(students_list):
-    
-    name=input("Name:")
-    dob=input("DOB:")
+def find_lowest_student(students_list):
+    lowest = None
+
+    for student in students_list:
+        if lowest is None or student["Average"] < lowest["Average"]:
+            lowest = student
+    return lowest
+
+def find_highest_student(students_list):
+    highest = None 
+
+    for student in students_list:
+        if highest is None or student["Average"] > highest["Average"]:
+            highest = student
+    return highest
+
+def insert_student(students_list):
+    name = input("Name:")
+    dob = input("DOB:")
 
     marks_list = []
-    
+
     while True:
         try:
             user_input = input("Enter 5 marks:")
             marks_list = list(map(int, user_input.split()))
             if len(marks_list) != 5:
-                print("ERROR - Enter 5 marks exactly")
-                continue
+                print("ERROR - Enter exactly 5 marks")
+                continue 
             if not validate_marks(marks_list):
-                print("ERROR - input marks between 0 - 100")
+                print("ERROR - Enter marks between 0 - 100")
                 continue 
             break
         except ValueError:
-                print("ERROR! - Do not enter charcaters - Only enter NUMBERS!")
-                continue
-           
-    average = find_average_marks(marks_list)
-    grade = final_grades(average)
+            print("ERROR - Do NOT enter characters - Enter ONLY numbers")
+            continue 
+    
+    av = find_average_mark(marks_list)
+    grade = final_grade(av)
 
     students_list.append({
         "Name":name,
-        "DOB":dob,
-        "Average":average,
+        "DOB": dob,
+        "Average":av,
         "Grade":grade
     })
-    print("Student successfully inserted")
-
-def lowest_student_marks(students_list):
-    lowest_student = None 
-
-    for student in students_list:
-        if lowest_student is None or student["Average"] < lowest_student["Average"]:
-            lowest_student = student
-    return lowest_student
-
-def highest_student_marks(students_list):
-    highest_student = None 
-
-    for student in students_list:
-        if highest_student is None or student["Average"] > highest_student["Average"]:
-            highest_student = student
-    return highest_student
+    print("Student inserted sucessfully")
 
 def display_students(students_list):
     if len(students_list) == 0:
-        print("No students found! - Please insert students")
+        print("No students found - Please first insert students and then search")
         return
     
     for s in students_list:
         print(f"Name:{s['Name']}")
         print(f"DOB:{s['DOB']}")
-        print(f"Average:{s['Average']}")
+        print(f"Final Mark(Average):{s['Average']}")
         print(f"Grade:{s['Grade']}")
     
-    lwt_std = lowest_student_marks(students_list)
-    hgt_std = highest_student_marks(students_list)
+    lwt_student = find_lowest_student(students_list)
+    hgt_student = find_highest_student(students_list)
 
-    print(f"Name:{lwt_std['Name']} Lowest Student:{lwt_std['Average']}")
-    print(f"Name:{hgt_std['Name']} Highest Student:{hgt_std['Average']}")
+    print(f"Name:{lwt_student['Name']} Lowest Mark:{lwt_student['Average']}")
+    print(f"Name:{hgt_student['Name']} Highest Mark:{hgt_student['Average']}")
 
-def choose_an_option(students_list):
+def search_students(students_list):
+    found = False
+
+    print("Student search engine")
+    user_input = input("Student name:").strip().lower()
+    for student in students_list:
+        if student["Name"].strip().lower() == user_input:
+            found = True
+            print(student)
+    if found == True:
+        print("Student sucessfully found")
+    else:
+        print("No student found")
+
+def delete_students(students_list):
+    found = False
+
+    print("Delete a student")
+    user_input = input("Student name:").strip().lower()
+
+    for student in students_list:
+        if student['Name'].strip().lower() == user_input:
+            students_list.remove(student)
+            found = True
+            print(f"Student:{student['Name']},successfully deleted")
+            break 
+    if not found:
+        print("ERROR - Please re-enter correct name\nor\nList Empty ")
+        
+def choosing(students_list):
     while True:
         menu()
-        user_choice = input("Select a number from the menu to continue:")
+
+        user_choice = input("Enter a choice from the menu:")
+
         if user_choice == "1":
-            insert_students(students_list)
+            insert_student(students_list)
         elif user_choice == "2":
             display_students(students_list)
         elif user_choice == "3":
-            print("Kind regards Head Master: MSc BSc ICT Yusuf Ahmad Ali\nGOODBYE")
-            break
-# Most Important
+            search_students(students_list)
+        elif user_choice == "4":
+            delete_students(students_list)
+        elif user_choice == "5":
+            print("Kindest Regards, GoodBye.\nHead Master MSc BSc DipICT Yusuf Ahmad Ali ")
+            break 
+        else:
+            print("ERROR - Invalid selection- Try again")
+            
+# IMPORTANT WITHOUT THIS PROGRAM WILL NOT RUN
 all_students = []
-choose_an_option(all_students)
+choosing(all_students)
+        
+
+
+
+
 
 
             
